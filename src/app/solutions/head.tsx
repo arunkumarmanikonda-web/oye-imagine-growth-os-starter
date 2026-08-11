b9132d80672f7c7d8e2e080229a3e717358db67b
arunkumarmanikonda-web <arunkumar.manikonda@gmail.com>
2026-08-11 20:38:33 +0530

    chore(ui22): remove obsolete SEO head files

--- src/app/solutions/head.tsx
+++
@@ -1,9 +0,0 @@
-﻿export default function Head() {
-  return (
-    <>
-      <title>Oye !magine Solutions | Search Visibility and Discoverability</title>
-      <meta name='description' content='Solutions overview for Oye !magine with search visibility, discoverability, and governed publishing surfaces.' />
-      <link rel='canonical' href='https://oyeimagine.com/solutions' />
-    </>
-  )
-}
